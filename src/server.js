3a309e09ca11d20b943c11db99495149807e22a0
chocodigo <cocoa1149@gmail.com>
2021-06-06 17:58:36 +0900

    + 20210606 about request, response

--- src/server.js
+++ src/server.js
@@ -4,6 +4,19 @@ const PORT = 4000;
 
 const app = express(); //express application을 만듦
 
+// application setting
+
+const handleHome = (req, res) => {
+  return res.send("I still love you");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
+};
+app.get("/", handleHome); // "/"으로 get request를 하면 callback함수를 받는다.
+app.get("/login", handleLogin);
+
+// 외부 접속 listen
 const handleListening = () =>
   console.log(`Server Listening on port http://localhost:${PORT} 🔈`);
 
