9e3ea30d1b87aedb5f5859a7302989610d2c1f92
chocodigo <cocoa1149@gmail.com>
2021-07-22 22:53:22 +0900

    + More Schema

--- src/models/Video.js
+++ src/models/Video.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
   //데이터 형식
-  title: String,
-  description: String,
+  title: { type: String, required: true, trim: true, maxLength: 80 },
+  description: { type: String, required: true, trim: true, minLength: 20 },
   createdAt: { type: Date, required: true, default: Date.now },
-  hashtags: [{ type: String }],
+  hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
