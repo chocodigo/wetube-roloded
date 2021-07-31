59f770ec39b7e798880589ea8ff1ecf1b5a964d7
chocodigo <cocoa1149@gmail.com>
2021-08-01 00:12:37 +0900

    + Logged In User. pug에서 session 처리중

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,27 +1,35 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 
-export const getJoin = (req, res) => res.render("join",{pageTitle: "Join"});
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, password2, location } = req.body;
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  const pageTitle = "Join";
 
-export const postJoin = async (req,res)=>{
-  
-  const {name,username,email,password,password2,location} = req.body;
-  const exists = await User.exists({$or: [{username},{email}]});
-  const pageTitle="Join";
-
-  if(password!==password2){
-    return res.status(400).render("join",{pageTitle,errorMessage : "Password confirmation does not match."});
-  }
-
-  if(exists){
-    return res.status(400).render("join",{pageTitle,errorMessage : "This username/email is already taken."});
-  }
-
- 
-  try{
-    await User.create({
-      name,username,email,password,location
+  if (password !== password2) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
     });
-  }catch (error) {
+  }
+
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+  } catch (error) {
     console.log(error);
     return res.status(400).render("join", {
       pageTitle: "Join",
@@ -35,18 +43,37 @@ export const edit = (req, res) => res.send("Edit User");
 
 export const remove = (req, res) => res.send("Remove User");
 
-export const getLogin = (req, res) => res.render("login",{pageTitle: "Login"});
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
 
 export const postLogin = async (req, res) => {
-  const{username, password}=req.body;
-  const exists = await User.exists({username});
-  if(!exists){
-    return res.status(400).render("login",{pageTitle:"Login",errorMessage: "An Account with this username does not exists."})
-  }
-  // check if account exists
-  // check if password correct
+  const { username, password } = req.body;
 
-  res.end();
+  const pageTitle = "Login";
+
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An Account with this username does not exists.",
+    });
+  }
+
+  const ok = await bcrypt.compare(password, user.password);
+
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password",
+    });
+  }
+
+  // 세션에 유저 데이터추가
+  req.session.loggedIn = true;
+  req.session.user = user;
+
+  console.log("LOG USER IN! COMING SOON!");
+  return res.redirect("/");
 };
 
 export const logout = (req, res) => res.send("Log out");
