601156a9f1363ff5c21872a827c2819ce1aee7de
최해림 <chocodigo1149@gmail.com>
2021-08-03 11:42:03 +0900

    + github login, logout 구현 완료

--- src/models/User.js
+++ src/models/User.js
@@ -1,20 +1,22 @@
 import mongoose from "mongoose";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
-const userSchema= new mongoose.Schema({
-    email:{type:String, required: true,unique:true},
-    username: {type: String, required:true,unique:true},
-    password: {type: String, required: true},
-    name:{type: String, requird: true},
-    location:String
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  avatarUrl: String,
+  socialOnly: { type: Boolean, default: false },
+  username: { type: String, required: true, unique: true },
+  password: { type: String },
+  name: { type: String, requird: true },
+  location: String,
 });
 
-userSchema.pre('save',async function(){
-    // console.log('Users password: ',this.password);
-    this.password = await bcrypt.hash(this.password,5);
+userSchema.pre("save", async function () {
+  // console.log('Users password: ',this.password);
+  this.password = await bcrypt.hash(this.password, 5);
 
-    // console.log("hashed password : ",this.password);
-})
+  // console.log("hashed password : ",this.password);
+});
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
