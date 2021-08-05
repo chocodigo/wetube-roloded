455279498b5ece4f478701a36473b00eae992efe
최해림 <chocodigo1149@gmail.com>
2021-08-05 17:17:42 +0900

    + Edit Profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -165,6 +165,25 @@ export const see = (req, res) => {
 export const getEdit = (req, res) => {
   return res.render("edit-profile", { pageTitle: "Edit Profile" });
 };
-export const postEdit = (req, res) => {
-  return res.render("edit-profie");
+export const postEdit = async (req, res) => {
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { name, email, username, location },
+  } = req;
+
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      name,
+      email,
+      username,
+      location,
+    },
+    { new: true }
+  );
+  //session에도 업데이트
+  req.session.user = updatedUser;
+  return res.redirect("/users/edit");
 };
