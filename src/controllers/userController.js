d0464193614feae761548c155da66c676c2fd0a5
reamhae <chocodigo>
2021-09-05 15:42:40 +0900

    + User's Videos 하는 중

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -229,10 +229,8 @@ export const see = async (req, res) => {
     return res.status(404).render("404", { pageTitle: "User not fuond." });
   }
 
-  const videos = await Video.find({ owner: user._id });
   return res.render("users/profile", {
     pageTitle: `${user.username}`,
     user,
-    videos,
   });
 };
