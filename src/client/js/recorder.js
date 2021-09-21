65759ef344c5ad93bac458502da50151820a68cd
chocodigo <cocoa1149@gmail.com>
2021-09-21 13:07:25 +0900

    + Download Transcoded Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -10,7 +10,7 @@ let videoFile;
 const handleDownload = async () => {
   const ffmpeg = createFFmpeg({
     log: true,
-    corePath: "/static/ffmpeg-core.js",
+    corePath: "https://unpkg.com/@ffmpeg/core@0.8.5/dist/ffmpeg-core.js",
   });
   await ffmpeg.load(); // 사용자가 소프트웨어를 사용할것이기 때문에 (소프트웨어가 무거울수 있기 때문에)
 
@@ -18,11 +18,17 @@ const handleDownload = async () => {
   ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
   // ffmpeg 명령어 (ecording.webm를 input으로 받기)
   // 초당 60프레임으로 인코딩
-  await ffmpeg.run("-i", "ecording.webm", "-r", "60", "output.mp4");
+  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+
+  const mp4File = ffmpeg.FS("readFile", "output.mp4");
+
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+
+  const mp4Url = URL.createObjectURL(mp4Blob);
 
   const a = document.createElement("a");
-  a.href = videoFile;
-  a.download = "MyRecording.webm"; // 해당 URL을 다운로드하는 것
+  a.href = mp4Url;
+  a.download = "MyRecording.mp4"; // 해당 URL을 다운로드하는 것
   document.body.appendChild(a);
   a.click();
 };
