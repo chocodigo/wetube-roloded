8a7eeac19ba2a33b2469e3f1caedc2e825dcb958
chocodigo <cocoa1149@gmail.com>
2021-09-23 17:12:47 +0900

    + Thumbnail Upload part one

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,34 @@
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 
-const startBtn = document.getElementById("startBtn");
+const actionBtn = document.getElementById("actionBtn");
 const video = document.getElementById("preview");
 
 let stream;
 let recorder;
 let videoFile;
 
+const files={
+  input: "recording.webm",
+  output: "output.mp4",
+  thumb:"thumbnail.jpg"
+}
+
+const downloadFile = (fileUrl, fileName)=>{
+  const a = document.createElement("a");
+  a.href = fileUrl;
+  a.download = fileName; // 해당 URL을 다운로드하는 것
+  document.body.appendChild(a);
+  a.click();
+}
+
 const handleDownload = async () => {
+
+  actionBtn.removeEventListener("click",handleDownload);
+
+  actionBtn.innerText = "Transcoding..."
+
+  actionBtn.disabled = true;
+
   const ffmpeg = createFFmpeg({
     log: true,
     corePath: "https://unpkg.com/@ffmpeg/core@0.8.5/dist/ffmpeg-core.js",
@@ -15,16 +36,16 @@ const handleDownload = async () => {
   await ffmpeg.load(); // 사용자가 소프트웨어를 사용할것이기 때문에 (소프트웨어가 무거울수 있기 때문에)
 
   // ffmpeg에 파일을 만들기
-  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+  ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
   // ffmpeg 명령어 (ecording.webm를 input으로 받기)
   // 초당 60프레임으로 인코딩
-  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+  await ffmpeg.run("-i", files.input, "-r", "60", files.output);
 
   // 특정시간으로 가서 스크린샷 한 장을 찍음
-  await ffmpeg.run("-i","recording.webm","-ss","00:00:01","-frames:v","1","thumbnail.jpg");
+  await ffmpeg.run("-i",files.input,"-ss","00:00:01","-frames:v","1",files.thumb);
 
-  const mp4File = ffmpeg.FS("readFile", "output.mp4");
-  const thumbFile = ffmpeg.FS("readFile","thumbnail.jpg");
+  const mp4File = ffmpeg.FS("readFile", files.output);
+  const thumbFile = ffmpeg.FS("readFile",files.thumb);
 
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
   const thumbBlob = new Blob([thumbFile.buffer],{type:"image/jpg"});
@@ -32,39 +53,33 @@ const handleDownload = async () => {
   const mp4Url = URL.createObjectURL(mp4Blob);
   const thumbUrl = URL.createObjectURL(thumbBlob);
 
-  const a = document.createElement("a");
-  a.href = mp4Url;
-  a.download = "MyRecording.mp4"; // 해당 URL을 다운로드하는 것
-  document.body.appendChild(a);
-  a.click();
+  downloadFile(mp4Url,"MyRecording.mp4");
+  downloadFile(thumbUrl,"MyThumbnail.jpg");
 
-  const thumbA = document.createElement("a");
-  thumbA.href = thumbUrl;
-  thumbA.download = "MyThumbnail.jpg"; // 해당 URL을 다운로드하는 것
-  document.body.appendChild(thumbA);
-  thumbA.click();
-
-  ffmpeg.FS('unlink','recording.webm');
-  ffmpeg.FS('unlink','output.mp4');
-  ffmpeg.FS('unlink','thumbnail.jpg');
+  ffmpeg.FS('unlink',files.input);
+  ffmpeg.FS('unlink',files.output);
+  ffmpeg.FS('unlink',files.thumb);
 
   URL.revokeObjectURL(mp4Url);
   URL.revokeObjectURL(thumbUrl);
   URL.revokeObjectURL(videoFile);
+  actionBtn.disabled = false;
+  actionBtn.innerText="Record Again";
 
+  actionBtn.addEventListener("click",handleStart);
 };
 
 const handleStop = () => {
-  startBtn.innerText = "Download Recording";
-  startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleDownload);
+  actionBtn.innerText = "Download Recording";
+  actionBtn.removeEventListener("click", handleStop);
+  actionBtn.addEventListener("click", handleDownload);
   recorder.stop();
 };
 
 const handleStart = () => {
-  startBtn.innerText = "Stop Recording";
-  startBtn.removeEventListener("click", handleStart);
-  startBtn.addEventListener("click", handleStop);
+  actionBtn.innerText = "Stop Recording";
+  actionBtn.removeEventListener("click", handleStart);
+  actionBtn.addEventListener("click", handleStop);
 
   recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
@@ -90,4 +105,4 @@ const init = async () => {
 
 init();
 
-startBtn.addEventListener("click", handleStart);
+actionBtn.addEventListener("click", handleStart);
