205a1ec6ceab7cd5611c5f564b17a31ad745f4b1
chocodigo <cocoa1149@gmail.com>
2021-09-23 16:46:14 +0900

    + Thumbnail

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -20,17 +20,29 @@ const handleDownload = async () => {
   // 초당 60프레임으로 인코딩
   await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
 
+  // 특정시간으로 가서 스크린샷 한 장을 찍음
+  await ffmpeg.run("-i","recording.webm","-ss","00:00:01","-frames:v","1","thumbnail.jpg");
+
   const mp4File = ffmpeg.FS("readFile", "output.mp4");
+  const thumbFile = ffmpeg.FS("readFile","thumbnail.jpg");
 
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+  const thumbBlob = new Blob([thumbFile.buffer],{type:"image/jpg"});
 
   const mp4Url = URL.createObjectURL(mp4Blob);
+  const thumbUrl = URL.createObjectURL(thumbBlob);
 
   const a = document.createElement("a");
   a.href = mp4Url;
   a.download = "MyRecording.mp4"; // 해당 URL을 다운로드하는 것
   document.body.appendChild(a);
   a.click();
+
+  const thumbA = document.createElement("a");
+  thumbA.href = thumbUrl;
+  thumbA.download = "MyThumbnail.jpg"; // 해당 URL을 다운로드하는 것
+  document.body.appendChild(thumbA);
+  thumbA.click();
 };
 
 const handleStop = () => {
