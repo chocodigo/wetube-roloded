20a3adcae45411be28c4ae74988bed4c7e89a708
chocodigo <cocoa1149@gmail.com>
2021-09-26 22:04:32 +0900

    + Comment Models

--- src/models/Video.js
+++ src/models/Video.js
@@ -4,7 +4,7 @@ const videoSchema = new mongoose.Schema({
   //데이터 형식
   title: { type: String, required: true, trim: true, maxLength: 80 },
   fileUrl: { type: String, required: true },
-  thumbUrl : { type: String, required: true },
+  thumbUrl: { type: String, required: true },
   description: { type: String, required: true, trim: true, minLength: 2 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
@@ -12,6 +12,7 @@ const videoSchema = new mongoose.Schema({
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
   },
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
   owner: { type: mongoose.Schema.Types.ObjectId, requried: true, ref: "User" },
 });
 
